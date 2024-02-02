54f7181c2c0e03927b4738889bdb4b9e471223be
diego-carc <diegocar@lcg.unam.mx>
2024-02-02 09:08:11 -0600

    Notes R/smokingMice.R

---
+++ R/smokingMice.R
@@ -0,0 +1,32 @@
+# Data Normalization
+# CPM (Count per million)
+# CPM = read counts of gene * 10^6 / Total mapping
+# Scale log2(CPM + 0.5)
+
+# Filter non expressed genes
+
+# Quality control
+# Delete bad quality samples
+# Metrics as mitoRate and rRNA_Rate
+  # We expect the rRNA read to be low
+  # We expect mitocondrial RNA to be low, indicating
+  # the cells were properly lised.
+
+# Detect outliers
+
+# PCA
+  # Curation
+
+# Variables correlation
+  # Identify if interest variable is correlated with another variable
+  # Delete correlated variables
+
+# Differential Expression
+# Get B coefficients
+# voom(
+# lmc()
+# eBayes()
+
+# BoxPlots DEGs
+
+|
