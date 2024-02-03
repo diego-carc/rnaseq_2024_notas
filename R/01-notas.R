bf42be673b3c925c684f7423a9ad8153c66ffc4b
diego-carc <diegocar@lcg.unam.mx>
2024-02-03 15:46:16 -0600

    Improved 01_notas.R

--- R/01-notas.R
+++ R/01-notas.R
@@ -7,12 +7,12 @@ usethis::use_r("01-notas.R")
 # Second activity
 usethis::use_r("02-visualizar-mtcars.R")
 
-
 # Init git repo
 usethis::use_git()
 usethis::use_github()
 
 # Bioconductor notes
+usethis::use_r("03-Bioconductor.R")
 
 # Gene expression Bioconductor data structures
 usethis::use_r("04-Exp-Bioc-Struct.R")
@@ -23,11 +23,14 @@ usethis::use_r("05_recount3.R")
 # Make my app
 usethis::use_r("app.R")
 
-# Explore Mode Matrix
+# Explore Model Matrix
 usethis::use_r("06-ExploreModelMtrx.R")
 
+# Smoking mouse
 usethis::use_r("smokingMice.R")
 
+# Differential Expression analysis
 usethis::use_r("07_limma.R")
 
+# Last activity
 usethis::use_r("08_notas_repaso.R")
